cc39f069e4f21b606abe98e6c390e03f41caa0a8
linzor <94228496+linzor@users.noreply.github.com>
2021-11-14 19:55:50 +0100

    Add files via upload

---
+++ linzor-arch-full.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Fully Bloated Arch Linux installer.
+# Includes:
+# - customized GUI
+# - customized app config files
+# - additional browser extensions:
+#	- wappalyzer
+#	- Darkreader
+#	- Foxy Proxy
+#	- Tabliss
+#	- Gnome Shell Integration
+# - tons of wallpapers
+# - apps:
+#       + (GUI)
+#       - alacritty
+#       - brave browser
+#       - tor browser
+#       - pavu control
+#       - gnome-tweaks
+#	- tradingview (snap) {you will be asked if you want snaps. if not this app will not be installed}
+#	- Open Broadcast Software
+#	- Signal-Desktop
+#	- Tutanota-Desktop
+#	- Mailspring {you will be asked if you want this app}
+#	- Spotify (adblock) {you will be asked if you want this app}
+#	- TIDAL (nativefied) {you will be asked if you want this app}
+#	- Element
+#	- Steam
+#	- Discord {you will be asked if you want this app}
+#	- Lutris
+#	- OnlyOffice
+#	- VLC
+#	- Transmission
+#	- Rhythmbox
+#
+#       + (CLI)
+#       - wget
+#       - git
+#	- nativefier {you will be asked if you want this app}
+#	- npm
+#	- pip
+#	- paru
+#	- alacritty-themes
+#	- pulseaudio-dlna
+#	- cyt
+#	- s9
+#	- time-update
+#	- bashcat
+#	- lolcat
+#	- figlet
+#	- mpv
+#	- youtube-dl
+#
